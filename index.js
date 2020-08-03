0b53f35ceaeb4c63f527141bce891122c0c64d44
Bradly Horn <wolfnchaos@gmail.com>
2020-08-03 03:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ function theBeatlesPlay(musicians, instruments) {
 
   return theBeatlesPlay
 }
+
+function johnLennonFacts(array) {
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
+
+  let i = 0
+
+  while (i < facts.length) {
+    i += 1
+    array.push(fasts[i] + `!!!`)
+  }
+  return array
+}
