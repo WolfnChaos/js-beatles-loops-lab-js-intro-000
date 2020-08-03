4f6ef22f1cb05350c0e5b4c6024d393a983fa914
Bradly Horn <wolfnchaos@gmail.com>
2020-08-03 03:26:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   var theBeatlesPlay = []
 
   for (let i = 0; i < musicians.lenght; i++) {
-    theBeatlesPlay.push(musicians[i] `plays` instruments[i])
+    theBeatlesPlay.push(musicians[i] + `plays` + instruments[i])
   }
 
   return theBeatlesPlay
